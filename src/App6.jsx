18783a11bd3f72eb59091c2b2cff11dc0c470588
Jorge Macana <39782680+andresmacana@users.noreply.github.com>
2023-06-29 11:41:27 -0400

    correction to App

--- src/App6.jsx
+++ src/App6.jsx
@@ -1,14 +1,22 @@
-// App.jsx
-import React from "react";
+import React, { useState } from "react";
 import BusinessCardList from "./BusinessCardListFull6";
-import bizcards from "./Data";
+import initialBizcards from "./Data";
 import "./style.css";
 
 function App() {
+  const [bizcards, setBizcards] = useState(initialBizcards);
+
+  const handleUpdateBizcards = (updatedBizcards) => {
+    setBizcards(updatedBizcards);
+  };
+
   return (
     <div className="App">
       <h2>Business Cards</h2>
-      <BusinessCardList bizcards={bizcards} />
+      <BusinessCardList
+        bizcards={bizcards}
+        onUpdateBizcards={handleUpdateBizcards}
+      />
     </div>
   );
 }
